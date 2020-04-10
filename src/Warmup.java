326c64ce75a9e798c1c435fd07536e166b5bb1c7
Omri Bar Oz <baroz.omri@gmail.com>
2020-04-10 18:51:22 +0300

    asdasd

--- src/Warmup.java
+++ src/Warmup.java
@@ -1,20 +1,21 @@
+import java.util.Arrays;
+
 public class Warmup {
     public static int backtrackingSearch(int[] arr, int x, int fd, int bk, Stack myStack) {
-        int output=-1;
-        int counter =1;
-        for (int i=0;i<arr.length;i++){
+        int output = -1;
+        int counter = 1;
+        for (int i=0;i<arr.length;i++,counter++){
             myStack.push(i);
             if (arr[i]==x) {
                 output = i;
                 break;
             }
-            else if ((i+counter)%fd==0){
-                counter++;
-               for (int j=0;!myStack.isEmpty()&&j<bk;j++){
-                   i=(int)(myStack.pop());
-               }
+            else if (counter%fd==0){
+                for (int j = 0; !myStack.isEmpty() && j <= bk; j++)
+                    i = (int) (myStack.pop());
+                }
+                System.out.println(i);
             }
-        }
         return output;
     }
 
@@ -42,12 +43,16 @@ public class Warmup {
                 }
             }
             else{
-                int step = (int) stepstack.pop();
-                index = (int) myStack.pop();
-                if (step>=index)
-                    to = step;
+                int step=0;
+                if (!stepstack.isEmpty()) {
+                    step = (int) stepstack.pop();
+                }
+                if (!myStack.isEmpty())
+                    index = (int) myStack.pop();
+                if (step+1>=index)
+                    to = 2*index+from;
                 else
-                    from = step;
+                    from = 2*index-to;
                 }
             }
         return output;
@@ -61,5 +66,14 @@ public class Warmup {
         } else {
             return 0;
         }
+    }
+     public static void main(String[] args) {
+//        int[] arr = new int[100];
+//        for (int i=0;i<arr.length;i++){
+//            double res =Math.random()*100;
+//            arr[i]= (int)res%100;
+        int[] arr= {0,1,2,3,4,5,6,7,8,9,10};
+        Stack myStack = new Stack();
+         System.out.println(backtrackingSearch(arr,8,2,1,myStack));
     }
 }
