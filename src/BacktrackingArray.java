ef5c3a9463309f2992ee270da84c2dcf3b0256f0
NevoStrauss <nevostrauss@gmail.com>
2020-04-11 20:40:14 +0300

    backtracking array finished maddafakka

--- src/BacktrackingArray.java
+++ src/BacktrackingArray.java
@@ -11,21 +11,21 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
         arr = new int[size];
         minimum = Integer.MAX_VALUE;
         maximum = Integer.MIN_VALUE;
-        this.size=0;
+        this.size = 0;
     }
 
     @Override
-    public Integer get(int index){
-        // TODO: implement your code here
+    public Integer get(int index) {
+        return arr[index];
     }
 
     @Override
     public Integer search(int x) {
         Integer output = -1;
-        if (x<minimum | x>maximum)
+        if (x < minimum | x > maximum)
             return output;
-        for (int i=0;i<size;i++){
-            if (arr[i]==x) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] == x) {
                 output = i;
                 break;
             }
@@ -34,28 +34,37 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
     }
 
     @Override
-    public void insert(Integer x) {         //if (insert) push inserted value & "insert"
-                                            // remember minimum & maximum
-                                            // increase size
-        // TODO: implement your code here
+    public void insert(Integer x) {
+        stack.push(minimum);
+        stack.push(maximum);
+        stack.push("insert");
+        arr[size] = x;
+        size++;
+        if (x < minimum) {//if x is new minimum
+            minimum = x;
+        }
+        if (x > maximum) {//if x is new maximum
+            maximum = x;
+        }
     }
 
+
     @Override
-    public void delete(Integer index) { //push deleted value + "delete"
-                                        //decrease size
-                                        // arr[index]==min|max, min|max=pre|successor
-        if (index>=size | index<0)
-            throw new IllegalArgumentException("index out of bounds");
+    public void delete(Integer index) {
         stack.push(arr[index]);
+        stack.push(minimum);
+        stack.push(maximum);
+        stack.push(index);
         stack.push("delete");
         Integer temp = arr[index];
-        if (temp==minimum)
-            minimum=successor(index);
-        else if (temp==maximum)
-            maximum=predecessor(index);
-        arr[index]=arr[size-1];
-        arr[size-1]=0;
-        size=size-1;
+        if (temp == minimum)
+            minimum = successor(index);
+        else if (temp == maximum) {
+            maximum = predecessor(index);
+        }
+        arr[index] = arr[size - 1];
+        arr[size - 1] = 0;
+        size--;
     }
 
     @Override
@@ -70,17 +79,48 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
 
     @Override
     public Integer successor(Integer index) {
-        // TODO: implement your code here
+        if (arr[index] == maximum) {
+            throw new RuntimeException("maximum, none successor");
+        }
+        Integer curr = maximum;
+        for (int i = 0; i < size; i++) {
+            if ((arr[i] > arr[index]) & (arr[i] < curr)) {
+                curr = arr[i];
+            }
+        }
+        return curr;
     }
 
     @Override
     public Integer predecessor(Integer index) {
-        // TODO: implement your code here
+        if (arr[index] == minimum()) {
+            throw new RuntimeException("minimum, none predecessor");
+        }
+        Integer curr = minimum;
+        for (int i = 0; i < size; i++) {
+            if ((arr[i] < arr[index]) & (arr[i] > curr)) {
+                curr = arr[i];
+            }
+        }
+        return curr;
     }
 
     @Override
-    public void backtrack() {             //Object = stack.pop, if equals "delete" insert(stack.pop), else delete(
-        // TODO: implement your cod e here
+    public void backtrack() {
+        String action = (String) stack.pop();
+        if (action.equals("insert")) {
+            arr[size - 1] = 0;
+            size--;
+            maximum = (Integer) stack.pop();
+            minimum = (Integer) stack.pop();
+        } else if (action.equals("delete")) {
+            Integer index = (Integer) stack.pop();
+            maximum = (Integer) stack.pop();
+            minimum = (Integer) stack.pop();
+            Integer value = (Integer) stack.pop();
+            arr[size] = arr[index];
+            arr[index] = value;
+        }
     }
 
     @Override
@@ -90,10 +130,21 @@ public class BacktrackingArray implements Array<Integer>, Backtrack {
 
     @Override
     public void print() {
-        String toprint = ""+arr[0];
-        for (int i=1; i<size;i++){
-            toprint= toprint + " " + arr[i];
+        for (int i = 0; i < size; i++) {
+            if (i == size - 1)
+                System.out.println(arr[i]);
+            else
+                System.out.print(arr[i] + " ");
         }
-        System.out.println(toprint);
+    }
+
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        BacktrackingArray A = new BacktrackingArray(st, 20);
+        for (int i = 0; i < 4; i++) {
+            A.insert(i);
+        }
+        System.out.println(A.predecessor(2));
+        System.out.println(A.successor(A.minimum));
     }
 }
